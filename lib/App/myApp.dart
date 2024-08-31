5aed5d7bfa227809b1df02fa37e3f1531ecc0cd7
Mark <markmarkmagdy1010@gmail.com>
2024-09-01 02:09:22 +0300

    update

--- lib/App/myApp.dart
+++ lib/App/myApp.dart
@@ -1,6 +1,5 @@
 import 'package:book_nest/Core/Resources/colorManager.dart';
 import 'package:book_nest/Core/Resources/routeManager.dart';
-import 'package:book_nest/Features/Splash/Screens/splashPage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -12,8 +11,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'BookNest',
-      theme: ThemeData(
-        useMaterial3: true,
+      theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
       ),
       routes: RouteManager.routes,
