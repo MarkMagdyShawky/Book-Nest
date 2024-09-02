e4b774d977b3e801cf5d92457279893d9aad3968
Mark <markmarkmagdy1010@gmail.com>
2024-09-02 22:31:44 +0300

    create custom app bar

---
+++ lib/Features/Home/Widgets/customAppBar.dart
@@ -0,0 +1,29 @@
+import 'package:book_nest/Core/Resources/fontManager.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          FontManager().LogoText(38),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
