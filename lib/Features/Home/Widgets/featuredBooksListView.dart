14b4a8f41eb2bdf45414a1d0e91e4b600009f475
Mark <markmarkmagdy1010@gmail.com>
2024-09-02 22:34:06 +0300

    create featured books list view

---
+++ lib/Features/Home/Widgets/featuredBooksListView.dart
@@ -0,0 +1,24 @@
+import 'package:book_nest/Features/Home/Widgets/featuredListItem.dart';
+import 'package:flutter/material.dart';
+
+import '../../../Core/Resources/pageDimensions.dart';
+
+class FreaturedBooksListView extends StatelessWidget {
+  const FreaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: PageDimensions().pageHeight(context) * 0.29,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: FeaturedListItem(),
+          );
+        },
+      ),
+    );
+  }
+}
