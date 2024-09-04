bd47aca081b6f092f34dbc7fde294adaa6ea4bb0
Mark <markmarkmagdy1010@gmail.com>
2024-09-04 03:30:00 +0300

    create font styles file

---
+++ lib/Core/Resources/styles.dart
@@ -0,0 +1,32 @@
+import 'package:book_nest/Core/Resources/constans.dart';
+import 'package:flutter/widgets.dart';
+
+abstract class Styles {
+  static const textStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGTSectraFine,
+  );
+
+  static const textStyle16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal, // semi bold
+  );
+
+  static const textStyle18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600, // semi bold
+  );
+
+  static const textStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGTSectraFine,
+  );
+
+  static const textStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+    fontFamily: kGTSectraFine,
+  );
+}
