3a411cc1e57d9f7ca31751522f8e6aa2fca47660
Mark <markmarkmagdy1010@gmail.com>
2024-09-06 02:20:31 +0300

    create seperate similer book list view

--- lib/Features/BookDetails/Screens/book_details_body.dart
+++ lib/Features/BookDetails/Screens/book_details_body.dart
@@ -2,6 +2,7 @@ import 'package:book_nest/Core/Resources/colorManager.dart';
 import 'package:book_nest/Core/Resources/pageDimensions.dart';
 import 'package:book_nest/Core/Resources/styles.dart';
 import 'package:book_nest/Features/BookDetails/Widgets/custom_book_details_appBar.dart';
+import 'package:book_nest/Features/BookDetails/Widgets/similer_boos_list_view.dart';
 import 'package:book_nest/Features/Home/Widgets/BookListWidgets/rationgWidgets.dart';
 import 'package:book_nest/Features/Home/Widgets/custom_image_item.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +14,60 @@ class BookDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              children: <Widget>[
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: PageDimensions().pageWidth(context) * 0.21),
+                  child: CustomImageItem(),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  "The Jungle Book",
+                  style: Styles.textStyle30,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  "Rudyard Kipling",
+                  style: Styles.textStyle18.copyWith(
+                    fontStyle: FontStyle.italic,
+                    color: kWhiteColor1.withOpacity(0.7),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+                RationgWidgets(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(height: 30),
+                // Book Actions (btns)
+                BookActions(),
+                const Expanded(
+                  child: SizedBox(height: 50),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "You can also like",
+                    style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                SizedBox(height: 10),
+
+                SimilerBooksListView(),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
       child: Column(
@@ -22,13 +77,13 @@ class BookDetailsBody extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: PageDimensions().pageWidth(context) * 0.21),
             child: CustomImageItem(),
           ),
-          SizedBox(height: 30),
+          const SizedBox(height: 30),
           Text(
             "The Jungle Book",
             style: Styles.textStyle30,
             textAlign: TextAlign.center,
           ),
-          SizedBox(height: 5),
+          const SizedBox(height: 5),
           Text(
             "Rudyard Kipling",
             style: Styles.textStyle18.copyWith(
@@ -37,14 +92,16 @@ class BookDetailsBody extends StatelessWidget {
             ),
             textAlign: TextAlign.center,
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           RationgWidgets(
             mainAxisAlignment: MainAxisAlignment.center,
           ),
-          SizedBox(height: 30),
+          const SizedBox(height: 30),
           // Book Actions (btns)
           BookActions(),
-          SizedBox(height: 50),
+          const Expanded(
+            child: SizedBox(height: 50),
+          ),
           Align(
             alignment: Alignment.centerLeft,
             child: Text(
@@ -56,27 +113,6 @@ class BookDetailsBody extends StatelessWidget {
 
           SimilerBooksListView(),
         ],
-      ),
-    );
-  }
-}
-
-class SimilerBooksListView extends StatelessWidget {
-  const SimilerBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: PageDimensions().pageHeight(context) * .166,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: CustomImageItem(),
-          );
-        },
       ),
     );
   }
---
+++ lib/Features/BookDetails/Widgets/similer_boos_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:book_nest/Core/Resources/pageDimensions.dart';
+import 'package:book_nest/Features/Home/Widgets/custom_image_item.dart';
+import 'package:flutter/material.dart';
+
+class SimilerBooksListView extends StatelessWidget {
+  const SimilerBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: PageDimensions().pageHeight(context) * .166,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: CustomImageItem(),
+          );
+        },
+      ),
+    );
+  }
+}
