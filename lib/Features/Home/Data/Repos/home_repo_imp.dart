6b7935fa89be88d11bf504106dc9bb343e92a603
Mark <markmarkmagdy1010@gmail.com>
2024-09-10 02:04:32 +0300

    implement featured books method

--- lib/Features/Home/Data/Repos/home_repo_imp.dart
+++ lib/Features/Home/Data/Repos/home_repo_imp.dart
@@ -27,8 +27,19 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(endPoint: 'volumes?q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['item']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
