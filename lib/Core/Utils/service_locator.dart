4e240921b235c01de5776c183197f6db8b35b04f
Mark <markmarkmagdy1010@gmail.com>
2024-09-11 01:39:24 +0300

    add get it to app

---
+++ lib/Core/Utils/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:book_nest/Core/Utils/api_service.dart';
+import 'package:book_nest/Features/Home/Data/Repos/home_repo_imp.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiService>()));
+}
