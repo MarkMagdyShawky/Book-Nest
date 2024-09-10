ae5bc6181fbf7125608be4629ee69fc82dcdcf5d
Mark <markmarkmagdy1010@gmail.com>
2024-09-10 20:38:19 +0300

    create newest books cubitd done

---
+++ lib/Features/Home/Presentation/Manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:book_nest/Features/Home/Data/Models/book_model/book_model.dart';
+import 'package:book_nest/Features/Home/Data/Repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => NewestBooksFailure(failure.errorMessage),
+      (books) => NewestBooksSuccess(books),
+    );
+  }
+}
